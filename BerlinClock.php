7bd410271db8d49e190f8b69d06425351e82c909
Baptiste Honnay <baptiste.honnay@student.vinci.be>
2020-10-23 19:29:46 +0200

    refactor - extract for loop into modifyFiveMinutes private method

--- BerlinClock.php
+++ BerlinClock.php
@@ -5,14 +5,7 @@ class BerlinClock {
 
     public function convert(int $heures, int $minutes, int $secondes):array {
         $res = self::emptyClock();
-        for($i=5;$i<=$minutes;$i+=5){
-            if($i===15){
-                $res[3][($i/5)-1] = "R";
-            }else{
-                $res[3][($i/5)-1] = "Y";
-            }
-        }
-
+        $res = $this->modifyFiveMinutes($minutes, $res);
         $res=$this->modifySingleMinutes($res, $minutes%5);
         return $res;
     }
@@ -30,6 +23,17 @@ class BerlinClock {
     private function modifySingleMinutes(array $clock, int $minutes): array {
         for ($i = 0; $i < $minutes; $i++) {
             $clock[4][$i] = "Y";
+        }
+        return $clock;
+    }
+
+    private function modifyFiveMinutes(int $minutes, array $clock): array {
+        for ($i = 5; $i <= $minutes; $i += 5) {
+            if ($i === 15) {
+                $clock[3][($i / 5) - 1] = "R";
+            } else {
+                $clock[3][($i / 5) - 1] = "Y";
+            }
         }
         return $clock;
     }
