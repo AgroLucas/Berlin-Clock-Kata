b0e30f044c1f5f5e41186454a9765f6b0a3a8ee4
AgroLucas <lucasagro1122@gmail.com>
2020-10-26 19:21:17 +0100

    add - test_convert_given23H59M59S_shouldReturnAllSingleMinuteLightsAnd1And2And4And5And7And8And10And11FiveMinutesLightsYellowAnd3And6And9FiveMinutesLightsAnd1To3SingleHourLightsAndAllFiveHoursLightsAndSecondsLightRed

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -377,4 +377,37 @@ class BerlinClockTest extends TestCase {
 
         $this->assertEquals($expected, $actual);
     }
+
+    //STEP 666 UTLIMATE TEST OF DOOM
+
+    public function test_convert_given23H59M59S_shouldReturnAllSingleMinuteLightsAnd1And2And4And5And7And8And10And11FiveMinutesLightsYellowAnd3And6And9FiveMinutesLightsAnd1To3SingleHourLightsAndAllFiveHoursLightsAndSecondsLightRed() {
+        $expected = $this->emptyClock;
+        $expected[0][0]="R";
+        $expected[1][0]="R";
+        $expected[1][1]="R";
+        $expected[1][2]="R";
+        $expected[1][3]="R";
+        $expected[2][0]="R";
+        $expected[2][1]="R";
+        $expected[2][2]="R";
+        $expected[3][0]="Y";
+        $expected[3][1]="Y";
+        $expected[3][2]="R";
+        $expected[3][3]="Y";
+        $expected[3][4]="Y";
+        $expected[3][5]="R";
+        $expected[3][6]="Y";
+        $expected[3][7]="Y";
+        $expected[3][8]="R";
+        $expected[3][9]="Y";
+        $expected[3][10]="Y";
+        $expected[4][0]="Y";
+        $expected[4][1]="Y";
+        $expected[4][2]="Y";
+        $expected[4][3]="Y";
+
+        $actual = $this->berlinClock->convert(23,59,59);
+
+        $this->assertEquals($expected, $actual);
+    }
 }
